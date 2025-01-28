fb2ba85a17993413abb30c9bb8c9a4b96cf683b8
임태호 <rktlrhrl531@naver.com>
2025-01-28 23:32:41 +0900

    [S12P11A108-150] fix: Apply builder pattern to member entity

--- backend/src/main/java/net/dutymate/api/entity/Member.java
+++ backend/src/main/java/net/dutymate/api/entity/Member.java
@@ -16,12 +16,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Member {
 
 	@Id
