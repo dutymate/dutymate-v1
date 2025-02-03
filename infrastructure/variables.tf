6431b5b865a78efd71b842208c4875f2e6865165
한종우 <jongwooo.han@gmail.com>
2025-02-03 12:19:56 +0900

    [S12P11A108-282] chore: Set terraform default variable

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,9 +1,11 @@
 variable "aws_profile" {
   description = "The profile name that you have configured in the file .aws/credentials"
   type        = string
+  default     = "dutymate-admin"
 }
 
 variable "aws_region" {
   description = "The AWS Region in which you want to deploy the resources"
   type        = string
+  default     = "ap-northeast-2"
 }
