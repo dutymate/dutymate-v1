42123dc8c7719d2fd563aacc6b62a53de1122391
Kim HyunJin <koujie11@gmail.com>
2025-02-03 12:23:55 +0900

    [S12P11A108-164] refactor: Refactor enum class type

--- backend/src/main/java/net/dutymate/api/enumclass/SkillLevel.java
+++ backend/src/main/java/net/dutymate/api/enumclass/SkillLevel.java
@@ -1,7 +1,16 @@
 package net.dutymate.api.enumclass;
 
+import lombok.Getter;
+
+@Getter
 public enum SkillLevel {
-	LOW,
-	MID,
-	HIGH
+	LOW("초급"),
+	MID("중급"),
+	HIGH("상급");
+
+	private final String value;
+
+	SkillLevel(String value) {
+		this.value = value;
+	}
 }
