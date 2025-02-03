99ee4f0f402c000deb7a1dda1775b908bbfedcde
한종우 <jongwooo.han@gmail.com>
2025-02-03 10:44:31 +0900

    [S12P11A108-194] chore: Migrate existing S3 bucket to terraform

---
+++ infrastructure/Modules/S3/main.tf
@@ -0,0 +1,42 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket        = var.bucket_name
+  
+  tags = {
+    environment = var.environment_name
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public-access" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket-policy" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.public-access
+  ]
+
+  policy = <<POLICY
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"AllowPublicReadWriteAccess",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":[
+        "s3:GetObject",
+        "s3:PutObject"
+      ],
+      "Resource":["arn:aws:s3:::${aws_s3_bucket.s3_bucket.id}/*"]
+    }
+  ]
+}
+POLICY
+}
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,3 +2,10 @@ provider "aws" {
   profile     = var.aws_profile
   region      = var.aws_region
 }
+
+module "s3" {
+  source = "./Modules/S3"
+
+  bucket_name      = "dutymate-bucket-${var.environment_name}"
+  environment_name = var.environment_name
+}
