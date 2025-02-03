126f8cbb8e6c5c3513044def213e739ce087f1ab
한종우 <jongwooo.han@gmail.com>
2025-02-03 12:05:19 +0900

    [S12P11A108-280] chore: Use terraform workspace

--- infrastructure/Modules/S3/main.tf
+++ infrastructure/Modules/S3/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket        = var.bucket_name
   
   tags = {
-    environment = var.environment_name
+    environment = terraform.workspace
   }
 }
 
--- infrastructure/Modules/S3/variables.tf
+++ infrastructure/Modules/S3/variables.tf
@@ -1,9 +1,4 @@
-variable "environment_name" {
-  description = "The name of the environment (e.g., dev, staging, prod)"
-  type        = string
-}
-
 variable "bucket_name" {
-  description = "The base name of the S3 bucket"
+  description = "The name of the S3 bucket"
   type        = string
 }
