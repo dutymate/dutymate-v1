b53266881c95b9a55bbf142716a39864020425f3
Kim HyunJin <koujie11@gmail.com>
2025-02-05 16:09:43 +0900

    [S12P11A108-165] feat: Add update wardmember info service

--- backend/src/main/java/net/dutymate/api/wardmember/service/WardMemberService.java
+++ backend/src/main/java/net/dutymate/api/wardmember/service/WardMemberService.java
@@ -1,9 +1,12 @@
 package net.dutymate.api.wardmember.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import net.dutymate.api.entity.Member;
+import net.dutymate.api.entity.WardMember;
 import net.dutymate.api.wardmember.repository.WardMemberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -16,7 +19,10 @@ public class WardMemberService {
 
 	@Transactional
 	public void updateWardMember(Long memberId, Member member) {
+
 		// memberId로 wardMember 찾기
-		// WardMember wardMember = wardMemberRepository.findByWardMemberId
+		WardMember wardMember = wardMemberRepository.findById(memberId).orElseThrow(() -> new ResponseStatusException(
+			HttpStatus.BAD_REQUEST, "유효한 병동 회원이 아닙니다."));
+
 	}
 }
