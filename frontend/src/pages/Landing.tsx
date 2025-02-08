67eec7b8a8ce4a960b15df1afe9f95a356fe7f4a
이재현 <angle0918@naver.com>
2025-02-08 18:56:59 +0900

    [S12P11A108-221] feat: Add CTA button to Landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,7 +1,21 @@
 import StartTemplate from "../components/templates/StartTemplate";
+import { Button } from "../components/atoms/Button";
+import { useNavigate } from "react-router-dom";
 
 const Landing = () => {
-	return <StartTemplate />;
+	const navigate = useNavigate();
+
+	const handleStart = () => {
+		navigate("/login");
+	};
+
+	return (
+		<StartTemplate>
+			<Button color="primary" size="lg" width="long" onClick={handleStart}>
+				시작하기
+			</Button>
+		</StartTemplate>
+	);
 };
 
 export default Landing;
