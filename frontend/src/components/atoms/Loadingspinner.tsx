543d1bf6f8a729c7c60a789dc8c4b1e1b0476d60
ksh0125 <kgwp2002@naver.com>
2025-02-13 14:03:22 +0900

    [S12P11A108-535] fix: npm run format fix

--- frontend/src/components/atoms/Loadingspinner.tsx
+++ frontend/src/components/atoms/Loadingspinner.tsx
@@ -1,18 +1,18 @@
 import { SyncLoader } from "react-spinners";
 
 const PageLoadingSpinner = () => {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <SyncLoader
-        color="#F5A281"
-        cssOverride={{}}
-        loading
-        margin={5}
-        size={12}
-        speedMultiplier={1}
-      />
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center min-h-screen">
+			<SyncLoader
+				color="#F5A281"
+				cssOverride={{}}
+				loading
+				margin={5}
+				size={12}
+				speedMultiplier={1}
+			/>
+		</div>
+	);
 };
 
-export default PageLoadingSpinner; 
+export default PageLoadingSpinner;
