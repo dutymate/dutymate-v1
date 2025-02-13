310af281f153a645a5425f4489834d663d963f27
한종우 <jongwooo.han@gmail.com>
2025-02-13 12:20:28 +0900

    [S12P11A108-530] chore: Set docdb param to disable tls

--- infrastructure/Modules/DocumentDB/main.tf
+++ infrastructure/Modules/DocumentDB/main.tf
@@ -8,14 +8,15 @@ resource "aws_docdb_subnet_group" "docdbsg" {
 }
 
 resource "aws_docdb_cluster" "docdb" {
-  engine                 = "docdb"
-  cluster_identifier     = "dutymate-docdb-cluster"
-  master_username        = var.mongodb_username
-  master_password        = var.mongodb_password
-  db_subnet_group_name   = aws_docdb_subnet_group.docdbsg.id
-  vpc_security_group_ids = [var.sg_mongodb_id]
-  skip_final_snapshot    = true
-  storage_encrypted      = false
+  engine                          = "docdb"
+  cluster_identifier              = "dutymate-docdb-cluster"
+  master_username                 = var.mongodb_username
+  master_password                 = var.mongodb_password
+  db_subnet_group_name            = aws_docdb_subnet_group.docdbsg.id
+  vpc_security_group_ids          = [var.sg_mongodb_id]
+  skip_final_snapshot             = true
+  storage_encrypted               = false
+  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.docdb_params.name
 
   tags = {
     Name = "dutymate-docdb-cluster"
@@ -30,5 +31,19 @@ resource "aws_docdb_cluster_instance" "docdb_instance" {
 
   tags = {
     Name = "dutymate-docdb-instance"
+  }
+}
+
+resource "aws_docdb_cluster_parameter_group" "docdb_params" {
+  name   = "dutymate-docdb-params"
+  family = "docdb5.0"
+
+  parameter {
+    name  = "tls"
+    value = "disabled"
+  }
+
+  tags = {
+    Name = "dutymate-docdb-params"
   }
 }
