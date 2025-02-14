ce27c6d771471ccbf0b1e858b1d1cbe176862751
한종우 <jongwooo.han@gmail.com>
2025-02-15 01:00:42 +0900

    [S12P11A108-592] fix: Correct access denied error

--- infrastructure/Modules/CloudFront/main.tf
+++ infrastructure/Modules/CloudFront/main.tf
@@ -34,6 +34,20 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
     }
   }
 
+  custom_error_response {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 10
+  }
+
+  custom_error_response {
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 10
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
